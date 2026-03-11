879b2f174bc94cf37999fddb35b9631266997e19
vignu-kotipalli <vignukotipalli4@gmail.com>
2026-03-11 13:07:25 +0530

    Fix API base URL routing to include /api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hrms-lite-b8lw.onrender.com',
+  baseURL: 'https://hrms-lite-b8lw.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
